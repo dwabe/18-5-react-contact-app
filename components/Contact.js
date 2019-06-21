444ed1b52f4e31354770aebc01e3b73cb012e71b
Bartek Błęcki <bartek@blecki.pl>
2019-06-21 12:44:53 +0200

    Update Contact.js

---
+++ components/Contact.js
@@ -0,0 +1,22 @@
+var Contact = React.createClass({
+    propTypes: {
+        item: React.PropTypes.object.isRequired,
+    },
+  
+    render: function() {
+        return (
+            React.createElement('div', {className: 'contactItem'},
+                React.createElement('img', {
+                    className: 'contactImage',
+                    src: 'https://www.iconfinder.com/icons/211605/download/png/256'
+                }),
+                React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+                React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+                React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+                    this.props.item.email
+                ) 
+            )
+        )
+    },
+});
+  
